fa5819c982e21f96cfbfca6a316f32f39f249f80
AlankarSaxena2712 <saxenaalankar42@gmail.com>
2020-07-17 10:31:33 +0530

    Insertion Complete all three procedures

--- linked_list.py
+++ linked_list.py
@@ -12,6 +12,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+
     # function to print the Linked List from the head
     def printlist(self):
         temp = self.head
@@ -21,8 +22,11 @@ class LinkedList:
             print(temp.data)
             temp = temp.next
 
+
     # Function to insert the node at the beginning of the linked list
-    def at_beginning(self, data):
+    def insert_at_beginning(self, data):
+
+        # Getting the address of the first node
         temp = self.head
 
         # if node is empty
@@ -37,9 +41,64 @@ class LinkedList:
             self.head = NewNode
 
 
+    # Function to insert the node at the end of the Linked List
+    def insert_at_end(self, data):
+
+        # Getting the address of the first node
+        temp = self.head
+
+        # If the Linked lIst is empty
+        if temp is None:
+            NewNode = Node(data)
+            self.head = NewNode
+
+        # If the Linked List has some nodes previously
+        else:
+
+            # Looping through the Linked List, to the LAST Node
+            while temp.next is not None:
+                temp = temp.next
+            # here we reached the last node after the loop ends
+            
+            # inserting the node at the end
+            NewNode = Node(data)
+            temp.next = NewNode
+
+
+    # Function to insert the node after a node(number of the node)
+    def insert_after(self, data, after_node):
+
+        # Getting the address of the first node
+        temp = self.head
+
+        # If the Linked List is Empty
+        if temp is None:
+            print("Node couldn't be inserted using this as the Linked List is Empty.")
+            return
+
+        # If the Linked List has some Node previously
+        else:
+            count = 0
+
+            # reaching the specific node
+            while count < after_node - 2:
+                temp = temp.next
+
+                # Condition to check if the Position Provided is Greater than the length of the Linked List
+                # than it will insert at the end of the Linked List
+                if temp.next == None:
+                    break
+                count = count + 1
+
+            # inserting the node at the specific position
+            NewNode = Node(data)
+            NewNode.next = temp.next
+            temp.next = NewNode
+
+
 # Main method
 if __name__ == '__main__':
-    
+
     # created empty linked list
     llist = LinkedList()
 
@@ -53,8 +112,16 @@ if __name__ == '__main__':
     second.next = third
 
     # Inserting at the Beginning of the Linked List
-    llist.at_beginning("WEB")
-    llist.at_beginning("WES")
+    llist.insert_at_beginning("WEB")
+    llist.insert_at_beginning("WES")
+
+    # Inserting at the End of the Linked List
+    llist.insert_at_end("DES")
+
+    # Inserting at a Specific Position in the Linked List
+    llist.insert_after("res", 4)
+    llist.insert_after("HELLO", 6)
+    llist.insert_after("HEL", 10)
 
     # printing the Linked List
     llist.printlist()
